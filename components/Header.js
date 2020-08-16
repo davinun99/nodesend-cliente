9f88b26ae7383f7ac72386c1377b6b72a1fdfb63
David N <nundavid99@gmail.com>
2020-08-16 13:02:04 -0400

    Creacion y primeros pasos del forntend

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+const Header = () => {
+    return (
+        <header className="py-8 flex-col md:flex-row items-center justify-between">
+            <img src="logo.svg" className="w-64 mb-8 md:mb-0" alt="Logo"/>
+
+        </header>
+    )
+}
+
+export default Header;
