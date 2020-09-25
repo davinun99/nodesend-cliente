2ca4c4251007f95adb3b5b82877fb24a5a627634
David Nuñez <nundavid99@gmail.com>
2020-09-25 12:48:01 -0400

    Funcionalidad de subida de archivos lista

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -1,10 +1,36 @@
-import React from 'react';
-
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import clienteAxios from '../config/axios';
 
 const Dropzone = () => {
+
+    const onDrop = useCallback(async(acceptedFiles) => {
+        //creando form data
+        const formData = new FormData();
+        formData.append('archivo', acceptedFiles[0]);
+        const resultado = await clienteAxios.post('/api/archivos', formData);
+        console.log(resultado.data);
+        
+    },[]);
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone( {onDrop} );
     return (
         <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-            <p>Dropzone aqui</p>
+            <div {...getRootProps({className: 'dropzone w-full py-32'})}>
+                <input className="h-100" {...getInputProps()} />
+                {
+                    isDragActive ? <p className="text-2xl text-center text-gray-600">
+                        Suelta el archivo
+                    </p>:
+                    <div className="text-center">
+                        <p className="text-2xl text-center text-gray-600">
+                            Arrastra un archivo aqui 
+                        </p>
+                        <button className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">
+                            Selecciona archivos para subir
+                        </button>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
